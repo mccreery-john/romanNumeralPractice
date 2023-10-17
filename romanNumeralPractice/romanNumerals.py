e79de46073edf3c74c57dcc3646af4d43b715dcf
mccreery-john <124418648+mccreery-john@users.noreply.github.com>
2023-10-17 04:13:54 -0400

    Update romanNumerals.py manually
    Manual commit since git is being weird and won't uninstall/update. Added decimal to roman numeral conversion and modified original roman numeral conversion.

--- romanNumeralPractice/romanNumerals.py
+++ romanNumeralPractice/romanNumerals.py
@@ -1,94 +1,117 @@
+constNumerals = ["I", "V", "X", "L", "C", "D", "M"]
+constValues = [1, 5, 10, 50, 100, 500, 1000]
 
 
-def roman():
-    while True:
-        print("\nEnter a number in Roman numerals or enter q to quit.")
-        numerals = input().upper()
+def roman(inputString):
+    ecNotNumeral = "Error: Non-Roman numeral found, please try again."
+    ecNonValidOrder = "Error: The Roman numerals are not in a valid order, please try again."
+    index = 0
+    total = 0
+    for currentNumeral in inputString:
 
-        if numerals == "Q" or numerals == "QUIT":       #Leave program
-            break
+        # Check if all characters are letters of roman numerals
 
-
-        # Check for numbers
-        if not numerals.isalpha():
-            print("Error: Decimal number found, please try again.")
-
-
+        if currentNumeral not in constNumerals:
+            return ecNotNumeral
         else:
-            constNumerals = ["I", "V", "X", "L", "C", "D", "M"]
-            constValues = [1, 5, 10, 50, 100, 500, 1000]
+            # Check if the order of the numerals makes sense
 
-            isNumeral = True
-            validOrder = True
-            index = 0
-            total = 0
-            for currentNumeral in numerals:
+            constIndex = constNumerals.index(currentNumeral)  # Get the index of x in the constNumerals
 
-                # Check if all characters are letters of roman numerals
+            if index != len(inputString) - 1:
+                nextConstIndex = constNumerals.index(inputString[index + 1])
 
-                if currentNumeral not in constNumerals:
-                    isNumeral = False
-                    break
-                else:
-                    # Check if the order of the numerals makes sense
-                    #
+                # Check if the numeral following the current one is not larger than two numerals up
+                if constIndex + 2 < nextConstIndex:
+                    return ecNonValidOrder
 
-                    constIndex = constNumerals.index(currentNumeral)         #Get the index of x in the constNumerals
+                elif constIndex < nextConstIndex and constIndex % 2 != 0:  # no VX or LC etc.
+                    return ecNonValidOrder
 
+                elif (index != 0 and currentNumeral >= inputString[index - 1]
+                      and nextConstIndex > constIndex):  # no IIV or IIX or IXL, etc.
+                    return ecNonValidOrder
 
-                    if index != len(numerals) - 1:
-                        nextConstIndex = constNumerals.index(numerals[index + 1])
+                elif currentNumeral == inputString[index + 1]:
+                    if constIndex % 2 != 0:  # no VV or LL or DD
+                        return ecNonValidOrder
 
-                        # Check if the numeral following the current one is not larger than two numerals up
-                        if constIndex + 2 < nextConstIndex:
-                            validOrder = False
-                            break
-                        elif constIndex < nextConstIndex and constIndex % 2 != 0:               #no VX or LC etc.
-                            validOrder = False
-                            break
-                        elif index != 0 and currentNumeral >= numerals[index - 1] and nextConstIndex > constIndex:       #no IIV or IIX or IXL, etc.
-                            validOrder = False
-                            break
-                        elif currentNumeral == numerals[index + 1]:
-                            if constIndex % 2 != 0:     #no VV or LL or DD
-                                validOrder = False
-                                break
-                            elif (index <= len(numerals) - 4 and currentNumeral == numerals[index + 2] and
-                                    currentNumeral == numerals[index + 3]):          #no IIII or XXXX etc.
-                                validOrder = False
-                                break
-                            else:
-                                total += constValues[constIndex]
-                        else:
-                            if constIndex < nextConstIndex:
-                                total -= constValues[constIndex]
-                            else:
-                                total += constValues[constIndex]
+                    elif (index <= len(inputString) - 4 and currentNumeral == inputString[index + 2] and
+                          currentNumeral == inputString[index + 3]):  # no IIII or XXXX etc.
+                        return ecNonValidOrder
+
                     else:
                         total += constValues[constIndex]
-                index += 1
-
-            # End "for currentNumeral in numerals" loop
-
-            if not isNumeral:
-                print("Error: Non-Roman numeral found, please try again.")
-            elif not validOrder:
-                print("Error: The Roman numerals are not in a valid order, please try again.")
+                else:
+                    if constIndex < nextConstIndex:
+                        total -= constValues[constIndex]
+                    else:
+                        total += constValues[constIndex]
             else:
-                print(total)
-    # End while True loop
+                total += constValues[constIndex]
+        index += 1
+
+    # End "for currentNumeral in numerals" loop
+    return total
+
 
 # ******************************* END roman function *******************************
 
 
+def decimal(inputString):
+    # Error Codes:
+    ecNonInt = "Error: Non-integer found, please try again."
+    ecNotInRange = "Error: Enter a valid integer between 1 and 3999."
+
+    # Check for int
+    try:
+        inputInt = int(inputString)
+    except TypeError:
+        return ecNonInt
+
+    # check for between 1 and 3999
+    if inputInt < 1 or inputInt > 3999:
+        return ecNotInRange
+
+    # Go by each decimal place and calculate the roman numeral(s) based on that
+    decimalPlace = 0
+    final = ""
+    for dec in reversed(inputString):
+        newNumeral = ""
+        dec = int(dec)  # Could throw error, but shouldn't since checked earlier
+        if dec <= 3:
+            newNumeral = dec * constNumerals[2 * decimalPlace]
+        elif dec == 4:
+            newNumeral = constNumerals[2 * decimalPlace] + constNumerals[(2 * decimalPlace) + 1]
+        elif dec == 5:
+            newNumeral = constNumerals[(2 * decimalPlace) + 1]
+        elif dec <= 8:
+            newNumeral = constNumerals[(2 * decimalPlace) + 1] + constNumerals[2 * decimalPlace] * (dec - 5)
+        elif dec == 9:
+            newNumeral = constNumerals[2 * decimalPlace] + constNumerals[2 * decimalPlace + 2]
+        # if dec == 0, keep newNumeral as ""
+
+        final = newNumeral + final
+        decimalPlace += 1
+
+    return final
 
 
-
-
+# ******************************* END decimal function *******************************
 
 
 if __name__ == '__main__':
-    roman()
 
+    while True:
+        print("\nEnter a valid Roman Numeral or Integer from 1 to 3999 or enter q to quit.")
+        newInput = input().upper()
 
+        if newInput == "Q" or newInput == "QUIT":  # Leave program
+            break
 
+        if newInput.isalpha():
+            print(roman(newInput))
+        elif newInput.isdecimal():
+            print(decimal(newInput))
+        else:
+            print("Error: Neither Roman Numeral or Integer entered")
